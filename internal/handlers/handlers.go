1b512afb3269ef2545c8ed0042d5fe203f91f576
Roman <rdymshakov@inbox.ru>
2024-01-16 17:36:34 +0300

    add render to make request with json and xml formates

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -13,27 +13,20 @@ func ShowIndexPage(ctx *gin.Context) {
 	// Get All articles
 	articles := models.GetAllArticles()
 
-	// Call the HTML method of the context to render a template
-	ctx.HTML(
-		// set the status ok (200)
-		http.StatusOK,
-		// Use the "index.html" template
-		"index.html",
-		//  pass the date that the page use
-		gin.H{
-			"title":   "Home Page",
-			"payload": articles,
-		},
-	)
+	// Call the render function with the name of template to render
+	Render(ctx,gin.H{
+		"title":"Home Page",
+		"payload":articles,
+	},"index.html")
 }
 
 func GetArticle(ctx *gin.Context) {
 	// Get article from url
-	articleId := ctx.Param("article_id") 
+	articleId := ctx.Param("article_id")
 	// Check if the article ID is valide
 	if articleIdInt, err := strconv.Atoi(articleId); err == nil {
 		// Check if the article exists
-		if article,err := models.GetArticleById(articleIdInt); err == nil {
+		if article, err := models.GetArticleById(articleIdInt); err == nil {
 			ctx.HTML(
 				// set http status ok (200)
 				http.StatusOK,
@@ -41,16 +34,30 @@ func GetArticle(ctx *gin.Context) {
 				"article.html",
 				// Pass the date that the page use
 				gin.H{
-					"title":article.Title,
-					"payload":article,
+					"title":   article.Title,
+					"payload": article,
 				},
 			)
 		} else {
 			// if the article is not found
-			ctx.AbortWithError(http.StatusNotFound,err)
+			ctx.AbortWithError(http.StatusNotFound, err)
 		}
 	} else {
 		// if article with this ID is not found
 		ctx.AbortWithStatus(http.StatusNotFound)
+	}
+}
+
+func Render(ctx *gin.Context, data gin.H, templateName string) {
+	switch ctx.Request.Header.Get("Accept") {
+	case "application/json":
+		// Respond with json
+		ctx.JSON(http.StatusOK, data["payload"])
+	case "application/xml":
+		// Repond with XML
+		ctx.XML(http.StatusOK, data["payload"])
+	default:
+		// Respond with HTML
+		ctx.HTML(http.StatusOK, templateName, data)
 	}
 }
